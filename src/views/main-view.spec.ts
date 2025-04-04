ff6ead4c7eb1b8f03ec0d5bf66440393b03daf61
Isabella Schepisi <schepisii2@gmail.com>
2025-04-04 12:00:25 -0500

    Add PDF

--- src/views/main-view.spec.ts
+++ src/views/main-view.spec.ts
@@ -3,8 +3,9 @@ import {shallowMount} from '@vue/test-utils';
 import {describe, it, expect} from 'vitest';
 
 describe('Main page', () =>{
- it('Shows disclaimer', () =>{
+ it('Shows disclaimer and pdf', () =>{
     const w = shallowMount(MainView);
     expect(w.find('[data-test-id="disclaimer-message"]').exists()).toBeTruthy();
+    expect(w.find('[data-test-id="chop-pdf"]').exists()).toBeTruthy();
  });
 });
