e8152fc3eafe4d83a7ccf41709f7777b71c1b25a
Isabella Schepisi <schepisii2@gmail.com>
2025-04-04 19:23:54 -0500

    Create month dropdown

--- src/views/main-view.spec.ts
+++ src/views/main-view.spec.ts
@@ -3,7 +3,7 @@ import {flushPromises, shallowMount} from '@vue/test-utils';
 import {describe, it, expect} from 'vitest';
 import DisclaimerInformation from '../components/disclaimer-information.vue';
 
-describe('Main page', () =>{
+describe('Disclaimer', () =>{
  it('does not show disclaimer on page load', () =>{
     const w = shallowMount(MainView);
     expect(w.find('[data-test-id="disclaimer-information"]').exists()).toBeFalsy();
@@ -26,4 +26,20 @@ describe('Main page', () =>{
    
    expect(w.find('[data-test-id="disclaimer-information"]').exists()).toBeFalsy();
  });
+});
+
+describe('Dropdowns', () => {
+   it('renders month dropdown', () => {
+      const w = shallowMount(MainView);
+      const expectedMonths = ['Month 1', 'Month 2', 'Month 3', 'Month 4', 'Month 5', 'Month 6', 'Month 7', 'Month 8'];
+      expect(w.find('[data-test-id="month-dropdown"]').exists()).toBeTruthy();
+      expectedMonths.forEach((month) => {
+         expect(w.find(`[data-test-id="${month}-dropdown-option"]`).exists()).toBeTruthy();
+      });
+   });
+   it('sets selected month on click', async () => {
+      const w = shallowMount(MainView);
+      await w.find('[data-test-id="Month 4-dropdown-option"]').trigger('click')
+      expect((w.vm as any).selectedMonth).toBe('Month 4');
+   });
 });
--- src/views/main-view.vue
+++ src/views/main-view.vue
@@ -1,15 +1,24 @@
 <script setup>
 import { ref } from 'vue';
 import DisclaimerInformation from '../components/disclaimer-information.vue';
+
 const showDisclaimer = ref(false);
+
+const selectedMonth = ref('');
+const months = ['Month 1', 'Month 2', 'Month 3', 'Month 4', 'Month 5', 'Month 6', 'Month 7', 'Month 8'];
 </script>
 
 <template>
   <disclaimer-information v-if="showDisclaimer" data-test-id="disclaimer-information" @close="showDisclaimer = false"/>
   <div v-else class="m-3">
-    <div class="d-flex justify-content-between">
+    <div data-test-id="main-header" class="d-flex justify-content-between">
       <h2>POTS Exercise Program Timer</h2>
       <button type="button" class="btn btn-secondary" data-test-id="disclaimer-button" @click="showDisclaimer=true">See Disclaimer</button>
+    </div>
+    <div data-test-id="main-body">
+      <select class="form-select form-select-sm my-3" data-test-id="month-dropdown">
+        <option v-for="month in months" v-bind:key="month" :data-test-id="month + '-dropdown-option'" @click="selectedMonth = month">{{ month }}</option>
+      </select>
     </div>
   </div>
 </template>
