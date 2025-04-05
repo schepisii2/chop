cdb77166c1d9043206d50b7016d9fcb020e51bf5
Isabella Schepisi <schepisii2@gmail.com>
2025-04-04 21:27:46 -0500

    Update dropdowns

--- src/views/main-view.vue
+++ src/views/main-view.vue
@@ -17,18 +17,30 @@ const days = computed(() => {
 <template>
   <disclaimer-information v-if="showDisclaimer" data-test-id="disclaimer-information" @close="showDisclaimer = false"/>
   <div v-else class="m-3">
+
     <div data-test-id="main-header" class="d-flex justify-content-between">
       <h2>POTS Exercise Program Timer</h2>
       <button type="button" class="btn btn-secondary" data-test-id="disclaimer-button" @click="showDisclaimer=true">See Disclaimer</button>
     </div>
-    <h3>{{ selectedMonth }} - Day {{ selectedDay }}</h3>
+    
     <div class="d-flex flex-row">
-      <select class="form-select form-select-sm me-2" data-test-id="month-dropdown">
-        <option v-for="month in months" v-bind:key="month" :data-test-id="month + '-dropdown-option'" @click="selectedMonth = selected">{{ month }}</option>
-      </select>
-      <select class="form-select form-select-sm" data-test-id="day-dropdown">
-        <option v-for="day in days" v-bind:key="day" :data-test-id="day + '-dropdown-option'" @click="selectedDay = day">Day {{ day }}</option>
-      </select>
+      <div class="dropdown me-2" data-test-id="month-dropdown">
+        <button class="btn btn-outline-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          {{ selectedMonth }}
+        </button>
+        <ul class="dropdown-menu">
+          <li v-for="month in months" v-bind:key="month" :data-test-id="month + '-dropdown-option'" @click="selectedMonth = month"><a class="dropdown-item">{{ month }}</a></li>
+        </ul>
+      </div>
+
+      <div class="dropdown" data-test-id="day-dropdown">
+        <button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+          Day {{ selectedDay }}
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          <li v-for="day in days" v-bind:key="day" :data-test-id="day + '-dropdown-option'" @click="selectedDay = day"><a class="dropdown-item">Day {{ day }}</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
