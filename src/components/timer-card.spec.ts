ef888448fd313ca512438cfc570de5b8f577691c
Isabella Schepisi <schepisii2@gmail.com>
2025-04-10 17:23:33 -0500

    Add zone information to timer

--- src/components/timer-card.spec.ts
+++ src/components/timer-card.spec.ts
@@ -1,22 +1,23 @@
 import TimerCard from './timer-card.vue';
 import {shallowMount} from '@vue/test-utils';
 import {describe, it, expect, vi} from 'vitest';
+import { TrainingZone } from '../models/training-days';
 
 describe('Timer Card', () =>{
  it('Shows title', () => {
-    const w = shallowMount(TimerCard, { props: {duration: 10}});
-    expect(w.find('[data-test-id="timer-title"]').text()).toBe('10 Minute Timer');
+    const w = shallowMount(TimerCard, { props:  { duration: 10, zone: TrainingZone.WarmUp }});
+    expect(w.find('[data-test-id="timer-title"]').text()).toBe('10 Minute Timer - Warm Up');
  });
  it('Shows time remaining', () => {
-   const w = shallowMount(TimerCard, { props: {duration: 10}});
+   const w = shallowMount(TimerCard, { props:  { duration: 10, zone: TrainingZone.WarmUp}});
    expect(w.find('[data-test-id="time-remaining"]').text()).toBe('10:00');
  });
  it('Shows start button', () => {   
-   const w = shallowMount(TimerCard, { props: {duration: 10}});
+   const w = shallowMount(TimerCard, { props:  { duration: 10, zone: TrainingZone.WarmUp}});
    expect(w.find('[data-test-id="start-button"]').text()).toBe('Start');
  });
  it('Hides start button on-click', async () => {   
-   const w = shallowMount(TimerCard, { props: {duration: 10}});
+   const w = shallowMount(TimerCard, {props:  { duration: 10, zone: TrainingZone.WarmUp}});
    await w.get('[data-test-id="start-button"').trigger('click');
    expect(w.find('[data-test-id="start-button"]').exists()).toBeFalsy();
  });
@@ -24,12 +25,19 @@ describe('Timer Card', () =>{
    vi.useFakeTimers()
    const mockAlert = vi.fn()
    window.alert = mockAlert; 
-   const w = shallowMount(TimerCard, { props: {duration: 10}});
+   const w = shallowMount(TimerCard, {props:  { duration: 10, zone: TrainingZone.WarmUp}});
 
    await w.get('[data-test-id="start-button"').trigger('click');
    vi.advanceTimersByTime(6000000);
 
    expect(w.emitted('close')).toBeDefined();
    expect(mockAlert).toHaveBeenCalled();
+ });
+ it('Shows zone information', () => {   
+   const w = shallowMount(TimerCard, { props:  { duration: 3, zone: TrainingZone.MSS}});
+   expect(w.find('[data-test-id="timer-title"]').text()).toBe('3 Minute Timer - Maximal Steady State (MSS)');
+   expect(w.find('[data-test-id="heart-rate"]').text()).toBe('Heart Rate: 165-175 BPM');
+   expect(w.find('[data-test-id="perceived-exertion"]').text()).toBe('Perceived Exertion: 5-8');
+
  });
 });
--- src/components/timer-card.vue
+++ src/components/timer-card.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { defineProps, defineEmits, ref } from 'vue';
 
-const props = defineProps(['duration'])
+const props = defineProps(['duration', 'zone'])
 const emits = defineEmits(['close'])
 
 const showStartButton = ref(true);
@@ -31,12 +31,14 @@ function startTimer() {
   <div class="card">
     <div class="card-header"> 
       <div class="d-flex justify-content-between">
-        <h5 class="card-title" data-test-id="timer-title">{{ props.duration }} Minute Timer</h5>
+        <h5 class="card-title" data-test-id="timer-title">{{ props.duration }} Minute Timer - {{ props.zone.label }}</h5>
       </div>
     </div>
     <div class="card-body d-flex flex-column">
        <h1 class="d-flex justify-content-center" data-test-id="time-remaining">{{ minutesLeft }}:{{ secondsLeft.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}}</h1> 
        <button v-if="showStartButton" type="button" class="btn btn-outline-dark" data-test-id="start-button" @click="startTimer">Start</button>
+       <hr v-if="props.zone.heartRate || props.zone.perceivedExertion" />
+       <div class="d-flex justify-content-around"><p v-if="props.zone.heartRate" data-test-id="heart-rate">Heart Rate: {{ props.zone.heartRate }} BPM</p> <p v-if="props.zone.perceivedExertion" data-test-id="perceived-exertion">Perceived Exertion: {{ props.zone.perceivedExertion }}</p></div>
     </div>
   </div>
 </template>
